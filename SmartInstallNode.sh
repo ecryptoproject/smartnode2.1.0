0a87820b9fa1104a6a06be632f70211cb672c0d4
ecryptoproject <48411435+ecryptoproject@users.noreply.github.com>
2024-01-09 10:49:05 +0100

    SmartInstallNode.sh

--- SmartInstallNode.sh
+++ SmartInstallNode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Downloading the wallet
 echo 'Downloading and extracting wallet files'
-wget "https://github.com/SmartCash/Core-Smart/releases/download/v2.1.0/smartcash-2.1.0-x86_64-linux-gnu.tar.gz" -O - | tar -xz
+wget "https://github.com/ecryptoproject/smartnode2.1.0/raw/main/smartcash-2.1.0-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 # Extract the files and give executable permissions
 echo 'Copying files to home directory' && sleep 1
